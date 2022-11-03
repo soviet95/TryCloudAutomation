5a814f4f87478690108e9eb8a4b636928366d36b
Vladimir Shilnikov <vladshil95@gmail.com>
2022-11-02 20:02:13 -0500

    Framework creation and first commit

---
+++ src/test/java/com/trycloud/utilities/BrowserUtils.java
@@ -0,0 +1,48 @@
+package com.trycloud.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class BrowserUtils {
+
+    public static void verifyTitle(WebDriver driver, String expectedTitle){
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(expectedTitle,actualTitle);
+    }
+
+    public static  void waitForInvisibility(WebElement element){
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 15);
+        wait.until(ExpectedConditions.invisibilityOf(element));
+    }
+
+    public static List<String> dropdownOptionsAsString(WebElement dropdown){
+        Select select = new Select(dropdown);
+        List<WebElement> actualOptionsAsWebElement = select.getOptions();
+        List<String> actualOptionsAsString = new ArrayList<>();
+        for (WebElement each : actualOptionsAsWebElement){
+            actualOptionsAsString.add(each.getText());
+        }
+        return actualOptionsAsString;
+    }
+
+    public static void clickRadioButton(List<WebElement> radioButtons, String attributeValue){
+        for (WebElement each : radioButtons){
+            if (each.getAttribute("value").equalsIgnoreCase(attributeValue)){
+                each.click();
+            }
+        }
+    }
+
+
+}
