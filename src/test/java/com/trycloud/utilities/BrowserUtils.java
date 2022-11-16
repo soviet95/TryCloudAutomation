9144c9bd3ec7145f79143dbb936bc0a27daf7ec7
vladimir d shilnikov <59296221+soviet95@users.noreply.github.com>
2022-11-15 23:36:34 -0600

    Update BrowserUtils.java

--- src/test/java/com/trycloud/utilities/BrowserUtils.java
+++ src/test/java/com/trycloud/utilities/BrowserUtils.java
@@ -1,18 +1,13 @@
 package com.trycloud.utilities;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class BrowserUtils {
 
